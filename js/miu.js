3c5eea6b428fd6b1da8459b9da0346772e6709b9
SFantasy <forever.fantasy27@gmail.com>
2013-04-17 21:26:10 +0800

    Add function: save file to local system

--- js/miu.js
+++ js/miu.js
@@ -65,7 +65,36 @@ function clearContent() {
  *  Save file function
  */
 function saveFile() {
-	var content = $('editor');
-	var uriContent = "data:application/octet-stream," + encodeURIComponent(conent.value);
-	window.open(uirContent, 'Save file as');
+	var fileName = prompt("Input file name: ");
+
+	/* 
+	Reference:
+	http://stackoverflow.com/questions/12718210/how-to-save-file-from-textarea-in-javascript-with-a-name 
+	*/
+	var Download = {
+	    click : function(node) {
+	        var ev = document.createEvent("MouseEvents");
+	        ev.initMouseEvent("click", true, false, self, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+	        return node.dispatchEvent(ev);
+	    },
+	    encode : function(data) {
+	            return 'data:application/octet-stream;base64,' + btoa( data );
+	    },
+	    link : function(data, name){
+	        var a = document.createElement('a');
+	        a.download = name || self.location.pathname.slice(self.location.pathname.lastIndexOf('/')+1);
+	        a.href = data || self.location.href;
+	        return a;
+	    }
+	};
+	Download.save = function(data, name){
+	    this.click(
+	        this.link(
+	            this.encode( data ),
+	            name
+	        )
+	    );
+	};
+
+	Download.save($("editor").value, fileName);
 }
